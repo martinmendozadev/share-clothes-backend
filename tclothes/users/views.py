f8de8a7ecc5a2be28b95d7defe4334ff9297079d
Martin Mendoza <marttcode@gmail.com>
2020-08-18 11:47:17 -0500

    Feat: Get Users Detail

--- tclothes/users/views.py
+++ tclothes/users/views.py
@@ -2,8 +2,11 @@
 
 # Django REST Framework
 from rest_framework.decorators import action
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, mixins
 from rest_framework.response import Response
+
+# Models
+from tclothes.users.models import User
 
 # Serializer
 from tclothes.users.serializers import (
@@ -12,11 +15,30 @@ from tclothes.users.serializers import (
     UserSignUpSerializer
 )
 
+# Permissions
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from tclothes.users.permissions import IsAccountOwner
 
-class UserViewSet(viewsets.GenericViewSet):
+
+class UserViewSet(viewsets.GenericViewSet,
+                  mixins.RetrieveModelMixin):
     """Users view set.
     Handle users login account.
     """
+
+    queryset = User.objects.all()
+    serializer_class = UserModelSerializer
+    lookup_field = 'username'
+
+    def get_permissions(self):
+        """Assign permissions based on action."""
+        if self.action in ['signup', 'login', 'verify']:
+            permissions = [AllowAny]
+        elif self.action in ['retrieve']:
+            permissions = [IsAuthenticated, IsAccountOwner]
+        else:
+            permissions = [IsAuthenticated]
+        return [p() for p in permissions]
 
     @action(detail=False, methods=['post'])
     def login(self, request):
