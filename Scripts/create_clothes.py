e068ee95796832b989e66f2062fefb9ae5c2fa54
Esteban Monguí <esteband.itorres@gmail.com>
2020-08-21 08:42:52 -0500

    Feat: Initial script for create clothes
    Initial structure for create csv file with random clothes

---
+++ Scripts/create_clothes.py
@@ -0,0 +1,27 @@
+"""Scipt to create clothes"""
+
+# Utilities
+import csv
+import random
+
+class Clothes()
+    def __init__(self):
+        pass
+    
+    def get_description(self):
+        pass
+    
+    def get_url_picture(self):
+        pass
+    
+    def choice_function(self, values_list):
+        pass
+    
+    def random_nuber(self):
+        pass
+
+def run():
+    pass
+
+if __name__ == '__main__':
+    def run()
