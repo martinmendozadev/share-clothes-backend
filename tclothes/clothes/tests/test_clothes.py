8e319dde0f2b241579446cd0b62d011c7bcc18e1
Esteban Monguí <esteband.itorres@gmail.com>
2020-08-23 08:56:09 -0500

    Fix: Clear test_clothes file

--- tclothes/clothes/tests/test_clothes.py
+++ tclothes/clothes/tests/test_clothes.py
@@ -2,33 +2,41 @@
 
 #Django
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 # Model
 from tclothes.clothes.models.clothes import ClothesModel
+from tclothes.clothes.models.interactions import InteractionsModel
 from tclothes.users.models.users import User 
 
 
-class ClothesModelTestCase(TestCase):
-    """Clothes model tests case."""
+class ClothesTestCase(TestCase):
+    """Clothes module tests case."""
+    
     def setUp(self):
         """Test case setup."""
-        self.user = User.objects.create(
+        self.owner = User.objects.create(
             username='6445968653',
             first_name='Sasha',
             last_name='Aristizabal',
             password='62Bv5eWQv6wM',
             email='sashaaristizabal@email.com'
         )
-
-    def tests_create_user_model(self):
-        clothe = ClothesModel.objects.create(
-            owner_is=self.user,
-            category='camiseta',
-            size='XXL',
-            color='beige',
-            gender='NS',
-            sell='False',
-            is_hide='True'
+        self.user = User.objects.create(
+            username='6459783474',
+            first_name='Mario',
+            last_name='Montolivo',
+            password='5bYqbB9EDZ3i77a',
+            email='mariomontolivo@email.com'
         )
-        import pdb; pdb.set_trace()
-        print(clothe)
+        self.clothe_context = {
+            owner_is:self.owner,
+            category:'camiseta',
+            size:'XXL',
+            color:'beige',
+            gender:'NS',
+            sell:'False',
+            is_hide:'True'
+        }
+
