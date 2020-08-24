a5ae7c4f180b5b4840e4152de3e7a78d1f82afa3
Martin Mendoza <marttcode@gmail.com>
2020-08-24 13:27:55 -0500

    Feat: Interactions clothes register admin panel

--- tclothes/clothes/admin.py
+++ tclothes/clothes/admin.py
@@ -4,7 +4,7 @@
 from django.contrib import admin
 
 # Models
-from tclothes.clothes.models import ClothesModel
+from tclothes.clothes.models import ClothesModel, InteractionsModel
 
 
 @admin.register(ClothesModel)
@@ -15,3 +15,9 @@ class ClothesAdmin(admin.ModelAdmin):
     search_fields = ('category', 'color', 'gender')
     list_filter = ('public', 'likes', 'dislikes', 'super_likes')
     readonly_fields = ['limit_pictures', 'likes', 'dislikes', 'super_likes']
+
+
+@admin.register(InteractionsModel)
+class InteractionsAdmin(admin.ModelAdmin):
+    """Interactions model admin."""
+    list_display = ('clothe', 'user', 'value')
