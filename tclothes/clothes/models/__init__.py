056d2f9c548e6655785b2418b4291460994bc6bd
Esteban Monguí <esteband.itorres@gmail.com>
2020-08-24 19:39:02 -0500

    Fix: fix conflicts of merge

--- tclothes/clothes/models/__init__.py
+++ tclothes/clothes/models/__init__.py
@@ -1,6 +1,2 @@
 from .clothes import ClothesModel
-<<<<<<< HEAD
-from .interactions import InteractionsModel
-=======
 from .matches import InteractionsModel
->>>>>>> a5ae7c4f180b5b4840e4152de3e7a78d1f82afa3
