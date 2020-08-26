22ef0bccc7617429476a40b9ef046c33be8518e1
Martin Mendoza <marttcode@gmail.com>
2020-08-26 13:53:01 -0500

    Fix: Phone number field
    New regex validation for phone number
    
    Only accept numbers into 10 from 15 digits

--- tclothes/clothes/serializers/clothes.py
+++
@@ -1,9 +0,0 @@
-"""Clothes serializer."""
-
-# Django REST framework
-from rest_framework import serializers
-
-
-class CreateClotheSerializer(serializers.ModelSerializer):
-    """Create new Clothe serializer."""
-    pass
